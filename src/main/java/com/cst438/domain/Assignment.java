e9033690677fc4777a3f69b949569b840b7ca50d
sevenwhiteclouds <kemaldonado@csumb.edu>
2024-03-10 17:13:25 -0700

    add missing components to assignment entity

--- src/main/java/com/cst438/domain/Assignment.java
+++ src/main/java/com/cst438/domain/Assignment.java
@@ -1,7 +1,6 @@
 package com.cst438.domain;
 
 import jakarta.persistence.*;
-
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 
@@ -11,9 +10,43 @@ public class Assignment {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="assignment_id")
     private int assignmentId;
- 
-    // TODO  complete this class
-    // add additional attributes for title, dueDate
-    // add relationship between assignment and section entities
-    // add getter and setter methods
+
+    @ManyToOne
+    @JoinColumn(name = "section_no", nullable = false)
+    private Section sectionNumber;
+
+    private String title;
+    private Date dueDate;
+
+    public int getAssignmentId() {
+        return assignmentId;
+    }
+
+    public void setAssignmentId(int assignmentId) {
+        this.assignmentId = assignmentId;
+    }
+
+    public Section getSectionNumber() {
+        return sectionNumber;
+    }
+
+    public void setSectionNumber(Section sectionNumber) {
+        this.sectionNumber = sectionNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
 }
