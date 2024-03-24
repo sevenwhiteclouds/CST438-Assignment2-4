24c93b037310da30e102dbd25bcf58bc2686996c
SMCode <steve@caseycontact.com>
2024-03-23 22:50:23 -0700

    Cont. of last commit, made mistake.

--- src/test/java/com/cst438/test/utils/Constants.java
+++ src/test/java/com/cst438/test/utils/Constants.java
@@ -1,11 +1,24 @@
+package com.cst438.test.utils;
+
 public enum Constants {
-    DRIVER_FILE_LOCATION(Constants.CHROME_DRIVER_FILE_LOCATION),
-    URL(Constants.URL),
-    SLEEP_DURATION(Constants.SLEEP_DURATION);
+    CHROME_DRIVER_FILE_LOCATION("<PATH HERE>/chromedriver-win64/chromedriver-win64/chromedriver.exe", 0),
+    URL("http://localhost:3000", 0),
+    SLEEP_DURATION("", 1000);
 
-    public static final String CHROME_DRIVER_FILE_LOCATION = "<PATH HERE>/chromedriver-win64/chromedriver-win64/chromedriver.exe";
-    public static final String URL = "http://localhost:3000";
-    public static final int SLEEP_DURATION = 1000; // milliseconds
+    private final String value;
+    private final int intValue;
 
+    Constants(String value, int intValue) {
+        this.value = value;
+        this.intValue = intValue;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public int getIntValue() {
+        return intValue;
+    }
 }
 
