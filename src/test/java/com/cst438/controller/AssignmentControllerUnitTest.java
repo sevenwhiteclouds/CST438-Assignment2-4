8a7b42ba1dec6817d75676e1025f21f876a025d1
Michelle Brown <michelleb.brown91@gmail.com>
2024-03-24 20:32:41 -0700

    Created unit test for: gradeAssignmentWithValidId

--- src/test/java/com/cst438/controller/AssignmentControllerUnitTest.java
+++ src/test/java/com/cst438/controller/AssignmentControllerUnitTest.java
@@ -1,5 +1,6 @@
 package com.cst438.controller;
 
+import com.cst438.dto.GradeDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @AutoConfigureMockMvc
@@ -19,6 +22,7 @@ public class AssignmentControllerUnitTest {
     @Autowired
     MockMvc mvc;
 
+    // Unit test to grade invalid assignment
     @Test
     public void getAssignmentGradesThrowsExceptionWithBadId() throws Exception {
 
@@ -38,6 +42,41 @@ public class AssignmentControllerUnitTest {
         // check the expected error message
         String message = response.getErrorMessage();
         assertEquals("assignment not found ", message);
+    }
+
+    // Unit test to grade assignment
+    @Test
+    public void gradeAssignmentWithValidId() throws Exception {
+
+        MockHttpServletResponse responseGet;
+        MockHttpServletResponse responsePut;
+
+        // issue the GET request
+        responseGet = mvc.perform(
+                        MockMvcRequestBuilders
+                                .get("/assignments/1/grades") // Give an assignmentId that does exist
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse();
+
+        // check the response code for 200 meaning OK
+        assertEquals(200, responseGet.getStatus());
+
+        // return data converted from String
+        List<GradeDTO> result = fromJsonString(responseGet.getContentAsString(), List.class);
+
+        // issue the PUT request
+        responsePut = mvc.perform(
+                        MockMvcRequestBuilders
+                                .put("/grades")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(asJsonString(result)))
+                .andReturn()
+                .getResponse();
+
+        // check the response code for 200 meaning OK
+        assertEquals(200, responsePut.getStatus());
     }
 
 
