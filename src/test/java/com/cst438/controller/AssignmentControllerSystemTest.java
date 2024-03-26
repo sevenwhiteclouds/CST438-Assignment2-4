8131c571792545024116766405b047b92bc624d6
Michelle Brown <michelleb.brown91@gmail.com>
2024-03-25 21:30:37 -0700

    Created System test to grade an assignment

--- src/test/java/com/cst438/controller/AssignmentControllerSystemTest.java
+++ src/test/java/com/cst438/controller/AssignmentControllerSystemTest.java
@@ -42,4 +42,64 @@ public class AssignmentControllerSystemTest {
             driver = null;
         }
     }
+
+    // System test to grade an assignment
+    @Test
+    public void systemTestGradeAssignment() throws Exception {
+
+        // Enter a Year and Semester
+        driver.findElement(By.id("year")).sendKeys("2024"); // Chose a valid year to enter
+        driver.findElement(By.id("semester")).sendKeys("Spring"); // Chose a valid semester to enter
+        driver.findElement(By.id("showSections")).click();
+        Thread.sleep(Constants.SLEEP_DURATION.getIntValue());
+
+        // Click "View Assignments" for first row (section) of table
+        // year and semester entered above for this test guarantees table results contain data
+        driver.findElement(By.id("viewAssignments")).click();
+        Thread.sleep(Constants.SLEEP_DURATION.getIntValue());
+
+        // Click "Show Grades" for first row (assignment) of table
+        // selecting the first section above for this test guarantees table results contain data
+        // with current data, there is only 1 grade record
+        driver.findElement(By.id("showGrades")).click();
+        Thread.sleep(Constants.SLEEP_DURATION.getIntValue());
+
+        // For the first Grade record in the table, save the current score in a variable
+        WebElement inputScore = driver.findElement(By.id("scoreInput"));
+        String currentScore = inputScore.getAttribute("value");
+
+        // Delete the score that is currently there
+        inputScore.sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.DELETE));
+
+        // Update the existing score to something other than what is currently there
+        driver.findElement(By.id("scoreInput")).sendKeys("30");
+        // Click "Update" for the first Grade record
+        driver.findElement(By.id("updateButton")).click();
+        Thread.sleep(Constants.SLEEP_DURATION.getIntValue());
+
+        // Assert message indicates new score was saved
+        String message = driver.findElement(By.id("addMessage")).getText();
+        assertTrue(message.startsWith("Grade saved"));
+
+        // Click "Back" to go back to list of Assignments
+        driver.findElement(By.id("backButton")).click();
+        Thread.sleep(Constants.SLEEP_DURATION.getIntValue());
+
+        // Click "Show Grades" again for first row (assignment) of table
+        driver.findElement(By.id("showGrades")).click();
+        Thread.sleep(Constants.SLEEP_DURATION.getIntValue());
+
+        // Confirm score saved as the new input above
+        // For the first Grade record in the table, it should show the new score that was entered
+        WebElement inputNewScore = driver.findElement(By.id("scoreInput"));
+        String newScore = inputNewScore.getAttribute("value");
+        assertEquals("30", newScore);
+
+        // Set score back to what it was before
+        WebElement inputScore2 = driver.findElement(By.id("scoreInput"));
+        inputScore2.sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.DELETE));
+        driver.findElement(By.id("scoreInput")).sendKeys(currentScore);
+        driver.findElement(By.id("updateButton")).click();
+        Thread.sleep(Constants.SLEEP_DURATION.getIntValue());
+    }
 }
